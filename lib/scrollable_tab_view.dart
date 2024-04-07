ce27a7acbb4602cdbac51d8a72b8d52aa1f1458e
shyam <shyamjith38@hotmail.com>
2024-04-07 13:31:42 +0530

    ver updates

--- lib/scrollable_tab_view.dart
+++ lib/scrollable_tab_view.dart
@@ -1,5 +1,3 @@
-/// This file exports several Dart files related to scrollable tab views and tab bars
-
 export './src/scrollable_tab_view_with_controller.dart'; // Exporting the file that contains a scrollable tab view with a controller
 export './src/scrollable_tab_bar.dart'; // Exporting the file that contains a scrollable tab bar
 export './src/scrollable_tab_view.dart'; // Exporting the file that contains a scrollable tab view
